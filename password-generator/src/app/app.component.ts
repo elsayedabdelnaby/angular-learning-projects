b0c6b9a85f1a23c2772182252d8ebbac2f1547c2
Elaraby <elsayed.fcis.1@gmail.com>
2021-08-31 15:15:24 +0200

    handel onchange options functions

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-generator';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  //handel on change use letters
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  //handel on change use numbers
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  //handel on change use symbols
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(){
+    this.password = 'MY PASSWORD!!';
+  }
 }
