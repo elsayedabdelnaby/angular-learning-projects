ed446ef559a0c2497b54ee16f23628e494e1f04c
Elaraby <elsayed.fcis.1@gmail.com>
2021-09-01 18:12:24 +0200

    passing data to component and display in app-root component

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy Mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking',
+      content: 'Here is a biking today'
+    },
+  ];
 }
