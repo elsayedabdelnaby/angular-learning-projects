821369143265930edad8e78bc0de34eea41365ae
unknown <elsayed.fcis.1@gmail.com>
2021-09-19 22:45:36 +0200

    generate appClass Custom Directive

---
+++ pages/src/app/class.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]'
+})
+export class ClassDirective {
+
+  constructor(private element: ElementRef) {
+  }
+
+  // @Input('appClass') set appClass(color: string){
+  //   this.element.nativeElement.style.backgroundColor = color;
+  // }
+
+  @Input('appClass') set classNames(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
+  }
+
+}
