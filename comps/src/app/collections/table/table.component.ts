cd35c81acddbd554a44da529ad0cc40ce7a90b9b
Elaraby <elsayed.fcis.1@gmail.com>
2021-11-04 16:36:53 +0200

    handle passing classes to the table component

--- comps/src/app/collections/table/table.component.ts
+++ comps/src/app/collections/table/table.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-  @Input() headers: any[] = [];
-  @Input() data: any[] = [];
+  //@Input() classNames: string; //this is mean when pass a parameter to TableComponent with classNames
+  @Input('class') classNames: string; //this is mean when pass a parameter to TableComponent with class
+  @Input() headers: any[];
+  @Input() data: any[];
 
-  constructor() {}
+  constructor() {
+    this.classNames = '';
+    this.headers = [];
+    this.data = [];
+  }
 
   ngOnInit() {}
 }
